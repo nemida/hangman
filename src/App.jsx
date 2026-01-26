4d16ed588ff5c4f3c3796cd3a1bb89fadcc7db1f
Nemida <144930141+Nemida@users.noreply.github.com>
2026-01-26 23:13:51 +0530

    feat: added correct/incorrect guess

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import { useState } from 'react';
 import { languages } from "./languages.js";
+import { clsx } from "clsx"
 export default function AssemblyEndgame() {
+
+    const [guessedLetters, setGuessedLetters] = useState([]);
+    // const [guessedLetters, setGuessedLetters] = useState(new Set()); // o(1) lookup
 
     const [currentWord, setCurrentWord] = useState("react");
 
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    const keyboardElements = alphabet.split("").map((elem, idx) => (
-        <button key={idx}>{elem.toUpperCase()}</button>
-    ));
+    function addGuessedLetters(letter) {
+        setGuessedLetters((prevLetters) =>
+            prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+        );
+        //setGuessedLetters((prevLetters) =>prevLetters.has(letter) ? prevLetters : new Set([...prevLetters, letter])
+        // if i use same set, it wont rerender it because the state reference remains the same
+    }
+
+    const keyboardElements = alphabet.split("").map((elem, idx) => {
+        const isGuessed = guessedLetters.includes(elem);
+        const isCorrect = isGuessed && currentWord.includes(elem);
+        const isWrong = isGuessed && !currentWord.includes(elem); 
+        
+        const className = clsx({
+            correct: isCorrect,
+            wrong: isWrong,
+
+        }) // applies the appropriate class ie gives the object with true values. they keys are the class name
+
+        return (
+            // based on the above conditions, you would have to do a complicated if condition. to stop that, we use clsx
+            <button className = {className} key={idx} onClick={() => addGuessedLetters(elem)}>{elem.toUpperCase()}</button>
+        )
+    });
 
     const letterElements = currentWord.split("").map((letter, index) => (
         <span key={index}>{letter.toUpperCase()}</span>
